16785339791a775fb0bffbfcbf8bc3cc62cd8644
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-03 16:54:05 +0100

    Constructor that creates a client to Redis

--- utils/redis.js
+++ utils/redis.js
@@ -2,41 +2,41 @@ import redis from 'redis';
 import {promisify} from 'util';
 
 class RedisClient {
-    constructor() {
-        // Creating the Redis client
-        this.client = redis.createClient();
+  constructor() {
+    // Creating the Redis client
+    this.client = redis.createClient();
 
-        // Handling error events from Redis
-        this.client.on('error', (err) => {
-            console.error('Redis client not connected to the server:', err);
-        });
+    // Handling error events from Redis
+    this.client.on('error', (err) => {
+        console.error('Redis client not connected to the server:', err);
+     });
 
-        // To Promisify the get, set, and del methods for asynchronous use
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
+    // To Promisify the get, set, and del methods for asynchronous use
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  // Checking if the Redis client is alive
+  isAlive() {
+    return this.client.connected;
+  }
+
+  // Getting a value from Redis based on a key
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
     }
 
-    // Checking if the Redis client is alive
-    isAlive() {
-        return this.client.connected;
-    }
+  // Setting a value in Redis with an expiration time
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
 
-    // Getting a value from Redis based on a key
-    async get(key) {
-        const value = await this.getAsync(key);
-        return value;
-    }
-
-    // Setting a value in Redis with an expiration time
-    async set(key, value, duration) {
-        await this.setAsync(key, value, 'EX', duration);
-    }
-
-    // Deleting a value in Redis based on a key
-    async del(key) {
-        await this.delAsync(key);
-    }
+  // Deleting a value in Redis based on a key
+  async del(key) {
+    await this.delAsync(key);
+  }
 }
 
 // Exporting an instance of RedisClient
