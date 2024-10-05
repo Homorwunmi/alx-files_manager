dce9bb5c89a71a8ac1694d24668d3efd0a12b51a
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-05 02:51:18 +0100

    Contains definition of the 2 endpoints

---
+++ controllers/AppController.js
@@ -0,0 +1,27 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static getStatus(req, res) {
+    // should check if Redis and DB are alive
+    const status = {
+        redis: redisClient.isAlive(),
+        db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    // should get the number of users and files
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    const stats = {
+        users,
+        files,
+    };
+    res.status(200).json(stats);
+  }
+}
+
+export default AppController;
