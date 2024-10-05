bb5058aa8c9f39d177361894738f4ce2525df588
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-05 03:09:31 +0100

    Adding a new endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 /*eslint-disable*/
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
+// App controller
 router.get('/status', AppController.getStatus);
-
 router.get('/stats', AppController.getStats);
+
+// User controller
+router.post('/users', UsersController.postNew);
 
 export default router;
