f9a5cb3a7180eba63066b70dd4d5614afd14cc15
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-10 10:49:40 +0100

    Adding a new endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,19 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
-// App controller
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// User controller
 router.post('/users', UsersController.postNew);
+
+// Authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+//User info route
+router.get('/users/me', UsersController.getMe);
 
 export default router;
