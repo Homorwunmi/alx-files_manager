23e445be0a4e8b28055ff1d314038c751a128161
Homorwunmi <omotayomowunmi@gmail.com>
2024-10-16 13:57:53 +0100

    Set up of the express server

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 /*eslint-disable*/
 import express from 'express';
-import routes from './routes/index';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-// Loading routes from routes/index.js
-app.use('/', routes);
+controllerRouting(app);
 
-// Starting the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
